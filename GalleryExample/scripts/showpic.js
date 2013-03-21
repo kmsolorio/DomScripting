203b0e7ecba14074529e982eaf5bc6d5cbfdbc34
Kevin Solorio <kevin@detone8.com>
2013-03-20 23:06:12 -0400

    update method to change p tag

--- GalleryExample/scripts/showpic.js
+++ GalleryExample/scripts/showpic.js
@@ -1,5 +1,14 @@
 function showPic(whichpic) {
   var source = whichpic.getAttribute('href');
+  var text = whichpic.getAttribute('title');
   var placeholder = document.getElementById('placeholder');
+  var description = document.getElementById('description');
   placeholder.setAttribute('src', source);
+  description.firstChild.nodeValue = text;
 };
+
+function countBodyChildren() {
+  var body_element = document.getElementsByTagName('body')[0];
+  alert(body_element.nodeType);
+};
+
