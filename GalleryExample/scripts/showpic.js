dce46fdad050f7fc65807b53538e3f8f17145e7c
Kevin Solorio <kevin@detone8.com>
2013-03-27 21:24:36 -0400

    add additional checks

--- GalleryExample/scripts/showpic.js
+++ GalleryExample/scripts/showpic.js
@@ -1,27 +1,48 @@
 function prepareGallery() {
   if (!document.getElementsByTagName) return false;
   if (!document.getElementById) return false;
-  if (!document.getElementById("imagegallery")) return false;
+  if (!document.getElementById('imagegallery')) return false;
 
-  var gallery = document.getElementById("imagegallery");
-  var links = gallery.getElementsByTagName("a");
+  var gallery = document.getElementById('imagegallery');
+  var links = gallery.getElementsByTagName('a');
 
   for (var i = 0; i < links.length; i ++) {
     links[i].onclick = function() {
-      showPic(this);
-      return false;
+      return !showPic(this);
     }
   }
 };
 
 
 function showPic(whichpic) {
+  if (!document.getElementById('placeholder')) return false;
+
   var source = whichpic.getAttribute('href');
-  var text = whichpic.getAttribute('title');
   var placeholder = document.getElementById('placeholder');
-  var description = document.getElementById('description');
+
+  if (placeholder.nodeName != "IMG") return false;
   placeholder.setAttribute('src', source);
-  description.firstChild.nodeValue = text;
+
+  if (document.getElementById('description')) {
+    var text = whichpic.getAttribute('title') ? whichpic.getAttribute('title') : "";
+    var description = document.getElementById('description');
+    if (description.firstChild.nodeType == 3) description.firstChild.nodeValue = text;
+  }
+
+  return true;
 };
 
-window.onload = prepareGallery;
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.oldonload != 'function') {
+    window.onload = func;
+  }
+  else {
+    window.onload = function() {
+      oldonload();
+      func();
+    }
+  }
+}
+
+addLoadEvent(prepareGallery);
